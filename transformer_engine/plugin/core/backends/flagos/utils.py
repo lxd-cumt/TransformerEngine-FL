a96b565dde1025b009f1fc52d74d4b349d0c0b1f
lxdcumt <1141051934@qq.com>
2026-01-12 12:30:48 +0800

    fix

--- transformer_engine/plugin/core/backends/flagos/utils.py
+++ transformer_engine/plugin/core/backends/flagos/utils.py
@@ -18,9 +18,11 @@ def gems_context():
     # Check if flagos should be enabled permanently via environment variable
     enable_flagos_permanently = os.getenv("TE_FL_ENABLE_FLAGOS_PERMANENTLY", "false").lower() in ("1", "true", "yes")
     if enable_flagos_permanently and not is_flag_gems_global_enabled:
-        flag_gems.enable(record=True, once=True)
+        flag_gems_log_path = os.getenv("TE_FL_FLAG_GEMS_LOG_PATH", None)
+        flag_gems_unused = os.getenv("TE_FL_FLAG_GEMS_UNUSED", None)
+        flag_gems.enable(record=True, once=True, unused=flag_gems_unused, path=flag_gems_log_path)
         is_flag_gems_global_enabled = True
-    
+
     # Use nullcontext if flag_gems is already globally enabled, otherwise use use_gems() context
     context = nullcontext() if is_flag_gems_global_enabled else flag_gems.use_gems()
     
